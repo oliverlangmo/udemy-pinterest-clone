3a669fee2b1072ec9742d3dbc5c680dd80c71bc3
oliverlangmo <oliver.langmo@gmail.com>
2016-11-03 21:28:51 -0500

    bundler update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,5 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'gentle-beyond-83585.herokuapp.com/'}
 end
-config.action_mailer.default_url_options = { host: 'gentle-beyond-83585.herokuapp.com/'}
